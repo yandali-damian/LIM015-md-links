26af2b85f54c09517a0c69ca8e856203ddb8e064
irwinet <ydamianelguera@gmail.com>
2021-07-26 09:52:40 -0500

    cambios de prueba

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// importando paquetes de node.js
+const chalk = require('chalk');
+const log = console.log;
+
 module.exports = () => {
     // ...
 };
+
+log(chalk.red('Hello world!'));
+log(chalk.blue('Hello') + ' World' + chalk.red('!'))
+
+
+console.log(chalk.bold.rgb(10, 100, 200)('Hello!'));
+console.log(chalk.bold.rgb(10, 100, 200)
+    `Hello!`);
+console.log(chalk `{bold.rgb(10,100,200) Hello!}`);
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+
+const name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
+
+const ctx = new chalk.Instance({ level: 0 });
